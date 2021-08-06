e7d8b3d620312897098e410342343ca4bcd32f7d
pablomabloo <oringinalatbest@gmail.com>
2021-08-06 12:04:18 -0400

    commit message here

---
+++ main.js
@@ -0,0 +1,12 @@
+let heading = document.getElementById('Baldmay');
+heading.innerHTML = "Kanye is the most trandescential piece of medium in our generation. Through his albums like the Life of Pablo, Yeezus, College Dropout, Ye has never dissapointed in his widely accalaimed discograpy of songs (except for Jesus is King). But I digress, the man has had over 21 grammys and many other awards towards his phase of experimenting. West is a rapper, singer, record producer, entrepreneur, politician, and fashion designer from the United States. You might've pointed out how he's a politician, he ran for presidency in 2020. Although he accumulated low numbers drastically compared to the other politicians, he showed that he is a versatile in his roles in throughtout his lifetime. Kanye is no stranger to controversy or heartbreak, after losing his mother in 2008, he made one of the most influential albums of the 21st century. My Beautiful Dark Twisted Fantasy, 808s and Heartbreaks, Yeezus, and so much more. Kanye is a man that has gone through a lot and has perservered through the troubles to never stop being himself.";
+heading.style["border"] = "2px solid black";
+heading.style["border-raidus"] = "12px";
+
+let og = document.getElementsByClassName("classespasses");
+for (let i = 0; i < og.length; i++){
+    og[i].style["border"] = "3px dotted black";
+    og[i].style["background-color"] = "white";
+    og[i].style["color"] = "black";
+}
+
